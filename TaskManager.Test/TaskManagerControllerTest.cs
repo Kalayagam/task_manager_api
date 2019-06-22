572651e1893ee9f4f650bfe143d9a54bc55d8eb6
Kalayagam <Kalayagam.rkm@gmail.com>
2019-06-22 18:32:11 +0530

    Added Unit Test Cases for Controller.

---
+++ TaskManager.Test/TaskManagerControllerTest.cs
@@ -0,0 +1,68 @@
+using System;
+using Xunit;
+using Moq;
+using TaskManager.Business;
+using System.Collections.Generic;
+using TaskManager.Core;
+using TaskManagerApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager.Test
+{
+    public class TaskManagerControllerTest
+    {
+        private readonly Mock<ITaskManagerBusiness> _mockTaskManager;
+        private readonly TaskManagerController _sut;
+
+        public TaskManagerControllerTest()
+        {
+            _mockTaskManager = new Mock<ITaskManagerBusiness>();
+            _sut = new TaskManagerController(_mockTaskManager.Object);
+        }
+
+        [Fact]
+        public async void GetAllTasksTest()
+        {
+            //Act
+            var mockTaskViewModel = new List<TaskViewModel>() {
+                new TaskViewModel()
+                {
+                   Id = 1,
+                   TaskName = "Task Name",
+                   EndDate = new DateTime(),
+                   StartDate = new DateTime(),
+                   ParentId = 0,
+                   ParentTaskName = null,
+                   Priority = 5
+                }
+            };
+
+            _mockTaskManager.Setup(x => x.GetAllTasks()).ReturnsAsync(mockTaskViewModel);
+
+            //Action
+            var response = await _sut.GetAllTasks();
+
+            //Assert
+            var oKObjectResult = Assert.IsType<OkObjectResult>(response);
+            var viewModel = Assert.IsAssignableFrom<List<TaskViewModel>>(oKObjectResult.Value);
+            Assert.Equal(mockTaskViewModel[0].Id, viewModel[0].Id);
+        }
+        [Fact]
+        public async void GetTaskTest()
+        {
+            var mockTaskViewModel =
+            new TaskViewModel()
+            {
+                Id = 1
+            };
+            _mockTaskManager.Setup(x => x.GetTask(It.IsAny<int>())).ReturnsAsync(mockTaskViewModel);
+            //Action
+            var response = await _sut.Get(mockTaskViewModel.Id);
+
+            //Assert
+            var oKObjectResult = Assert.IsType<OkObjectResult>(response);
+            var viewModel = Assert.IsAssignableFrom<TaskViewModel>(oKObjectResult.Value);
+            Assert.Equal(mockTaskViewModel.Id, viewModel.Id);
+        }            
+    }
+}
