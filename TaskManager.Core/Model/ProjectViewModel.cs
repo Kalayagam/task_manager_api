e03765f8100999a224a51d47c1fd2783608f3dc2
Kalayagam <Kalayagam.rkm@gmail.com>
2019-08-10 22:12:46 +0530

    make datetime nullable in project and task view models

--- TaskManager.Core/Model/ProjectViewModel.cs
+++ TaskManager.Core/Model/ProjectViewModel.cs
@@ -10,9 +10,9 @@ namespace TaskManager.Core.Model
 
         public string ProjectName { get; set; }
 
-        public DateTime StartDate { get; set; }
+        public DateTime? StartDate { get; set; }
 
-        public DateTime EndDate { get; set; }
+        public DateTime? EndDate { get; set; }
 
         public int Priority { get; set; }
 
--- TaskManager.Core/Model/TaskViewModel.cs
+++ TaskManager.Core/Model/TaskViewModel.cs
@@ -16,9 +16,9 @@ namespace TaskManager.Core
 
         public bool IsParentTask { get; set; }
 
-        public DateTime StartDate { get; set; }
+        public DateTime? StartDate { get; set; }
 
-        public DateTime EndDate { get; set; }
+        public DateTime? EndDate { get; set; }
 
         public int Priority { get; set; }
 
